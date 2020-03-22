810669898fa7aad6bd29edcac084006d8eb1447f
Szymon Warszawa <szymonwarszawa@gmail.com>
2020-03-22 10:25:10 +0000

    Added additional else if statement for control flow.

--- app.js
+++ app.js
@@ -38,4 +38,24 @@ switch (randomNumber) {
     break;
 }
 
+// if (randomNumber === 0) {
+//   eightBall = "It is certain";
+// } else if (randomNumber === 1) {
+//   eightBall = "It is decidedly so";
+// } else if (randomNumber === 2) {
+//   eightBall = "Reply hazy try again";
+// } else if (randomNumber === 3) {
+//   eightBall = "Cannot predict now";
+// } else if (randomNumber === 4) {
+//   eightBall = "Do not count on it";
+// } else if (randomNumber === 5) {
+//   eightBall = "My sources say no";
+// } else if (randomNumber === 6) {
+//   eightBall = "Outlook not so good";
+// } else if (randomNumber === 7) {
+//   eightBall = "Signs point to yes";
+// } else if (randomNumber === 8) {
+//   eightBall = "Yes!";
+// }
+
 console.log("The answer for your question is: ", eightBall);
